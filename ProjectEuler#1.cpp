328eec2fd704cef3964269bb528056b8ba915b16
Levi Hutchins <98019350+Levi-Hutchins@users.noreply.github.com>
2023-01-12 18:37:19 +1100

    Update ProjectEuler#1.cpp

--- ProjectEuler#1.cpp
+++ ProjectEuler#1.cpp
@@ -1,3 +1,9 @@
+/*
+  Majority of the following code can be found on HackerRank when 
+  attempting Problem number 1.
+  You are given sample code and need to write your own algorithm to pass
+  the test cases.
+*/
 #include <map>
 #include <set>
 #include <list>
@@ -23,7 +29,7 @@
 
 using namespace std;
 
-
+// Triangluar numbers formula
 unsigned long long test(unsigned long long x){ return x * (x + 1) / 2;}
 
 int main()
@@ -33,12 +39,15 @@ int main()
   while(num--){
       unsigned long long curr;
       cin >> curr;
-      curr--;
-      long divis_three   =  3 * test(curr /  3);
-      long divis_five    =  5 * test(curr /  5);
-      long divis_fifteen = 15 * test(curr / 15);
+      curr--; // 
+      long divis_three   =  3 * test(curr /  3); // Sum1 = {x : x % 3 == 0}
+      long divis_five    =  5 * test(curr /  5); // Sum2 = {x : x % 5 == 0}
+      long divis_fifteen = 15 * test(curr / 15); // Sum3 = {x : x % 15 == 0}
 
-    std::cout << (divis_three + divis_five - divis_fifteen) << std::endl;
+    
+      // Since 15 is both divisible by 3 and 5 we need to exclude any multiple of 15 from
+      // our total sum
+      cout << (divis_three + divis_five - divis_fifteen) << endl;
       
   }
   return 0;
